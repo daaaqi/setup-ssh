a96046cf3c9826b4bd653d52fdcc4c7e2d4f7d9b
朱大奇 <zhudq@kingnet.com>
2023-04-26 18:27:24 +0800

    脚本完善

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # setup-ssh
 自动贴公共钥且禁用ssh
+
 ```
 wget https://raw.githubusercontent.com/daaaqi/setup-ssh/main/setup-ssh.sh
 ```
+ 实现功能：
+
+* 检查用户是否为 root 账户
+* 检查是否安装 sudo，如果没有安装则安装 sudo
+* 检查是否已经禁用密码登录，如果已经禁用则无需再执行一次
+* 获取用户输入的公钥
+* 检查用户是否输入了公钥
+* 检查公钥格式是否正确
+* 将公钥贴到 authorized_keys 文件中
+* 禁用密码登录
+* 重启 sshd 服务
+* 检查已经存在的公钥
--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,58 +1,51 @@
 #!/bin/bash
 
-# Check for dependencies and install if missing
+# 检查用户是否为 root 账户
+if [ "$EUID" -ne 0 ]
+  then echo "请使用 root 账户执行此脚本"
+  exit
+fi
+
+# 检查是否安装 sudo
 if ! command -v sudo &> /dev/null
 then
-    echo "sudo command not found, installing..."
-    if command -v apt-get &> /dev/null
-    then
-        sudo apt-get update && sudo apt-get install -y sudo
-    elif command -v yum &> /dev/null
-    then
-        sudo yum install -y sudo
-    else
-        echo "Could not find a package manager to install sudo"
-        exit 1
-    fi
+    echo "sudo 未安装，现在开始安装"
+    apt-get update
+    apt-get install -y sudo
 fi
 
-# Determine whether to use sudo or not
-if [ "$(whoami)" == "root" ]
-then
-    SUDO=""
-else
-    SUDO="sudo"
+# 检查用户是否已经禁用密码登录
+if sudo grep -q "^PasswordAuthentication no" /etc/ssh/sshd_config; then
+  echo "已经禁用密码登录，无需再次操作"
+  exit
 fi
 
-# Step 1: Add SSH public key
-echo "Please enter your SSH public key:"
-read ssh_public_key
+# 获取用户输入的公钥
+echo "请粘贴您的公钥："
+read public_key
 
-# Validate SSH public key format
-if ! ssh-keygen -l -f <(echo "$ssh_public_key") >/dev/null 2>&1; then
-    echo "Invalid SSH public key format. Please enter a valid SSH public key."
-    exit 1
-elif [[ -z $ssh_public_key ]]; then
-    echo "SSH public key cannot be empty. Please enter a valid SSH public key."
-    exit 1
+# 检查用户是否输入了公钥
+if [ -z "$public_key" ]; then
+  echo "未输入公钥，脚本已停止"
+  exit
 fi
 
-mkdir -p ~/.ssh
-echo "$ssh_public_key" >> ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-
-# Step 2: Disable password authentication
-$SUDO sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-$SUDO sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
-$SUDO systemctl restart sshd
-
-# Step 3: Check for other SSH public keys in authorized_keys file
-echo "The following email addresses are currently authorized:"
-if [ -f ~/.ssh/authorized_keys ] && [ -s ~/.ssh/authorized_keys ]; then
-    awk '{print $NF}' ~/.ssh/authorized_keys | cut -d "@" -f 2 | sort | uniq
-else
-    echo "No other email addresses are currently authorized."
+# 检查公钥格式是否正确
+if ! echo "$public_key" | grep -q "ssh-rsa\|ssh-ed25519"; then
+  echo "公钥格式不正确，脚本已停止"
+  exit
 fi
 
-# Step 4: Print completion message
-echo "SSH public key has been added and password authentication has been disabled."
+# 将公钥贴到 authorized_keys 文件中
+sudo mkdir -p ~/.ssh
+echo "$public_key" | sudo tee -a ~/.ssh/authorized_keys > /dev/null
+
+# 禁用密码登录
+sudo sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
+
+# 重启 sshd 服务
+sudo systemctl restart sshd
+
+# 检查已经存在的公钥
+echo "当前机器上已经存在的公钥有："
+awk '{print $3}' < ~/.ssh/authorized_keys | cut -d '@' -f 2 | sort | uniq
